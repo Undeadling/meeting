2909382d995f8096ff3f03e5c2f67bb6b3688630
Undeadling <166484824+Undeadling@users.noreply.github.com>
2024-10-28 15:37:50 +0300

    Add files via upload

---
+++ Переменные исправленное.py
@@ -0,0 +1,5 @@
+complite_HW=12
+spend_hour=1.5
+curse_name="Python,"
+time= spend_hour/complite_HW
+print(curse_name,"всего задач:",complite_HW,'затрачено часов:',spend_hour,'среднее время выполнения',time)

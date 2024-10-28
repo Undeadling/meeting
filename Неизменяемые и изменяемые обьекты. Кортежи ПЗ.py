f0fab047f78b31babf4f874d761d988d2b4bdb37
Undeadling <166484824+Undeadling@users.noreply.github.com>
2024-10-28 16:43:15 +0300

    Add files via upload

---
+++ Неизменяемые и изменяемые обьекты. Кортежи ПЗ.py
@@ -0,0 +1,10 @@
+immutable_var=1,2,"a","b"
+print (immutable_var)
+#immutable_var[0]=3
+#print (immutable_var)
+mutable_list=[1,2,"a","b"]
+mutable_list.append(3)
+print (mutable_list)
+mutable_list.append("c")
+print (mutable_list)
+
